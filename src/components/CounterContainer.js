01d3846daa758a29dd0bf0d900eeaff6119b0bcc
aladka03 <aladka03@gmail.com>
2021-11-14 16:35:43 +0300

    created necessary files

---
+++ src/components/CounterContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CounterContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
