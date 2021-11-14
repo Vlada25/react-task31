b2cff5a72684b82586b63e003588a62cf72332ae
aladka03 <aladka03@gmail.com>
2021-11-14 22:40:35 +0300

    Created file with styles

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import PropTypes from 'prop-types';
+import styles from '../views/Counter/styles.js';
 
 function Counter({count, increment, decrement, reset}) {
     return (
-        <div className="counter">
-            <h2>My Counter</h2>
-            <h1>{count}</h1>
-            <div className="buttons">
+        <div style={styles.counter}>
+            <h2 style={styles.counter.h2}>My Counter</h2>
+            <h1 style={styles.counter.h1}>{count}</h1>
+            <div style={styles.buttonsContainer}>
                 <Button variant="outlined" onClick={increment}>Increment</Button>
                 <Button variant="outlined" onClick={decrement}>Decrement</Button>
                 <Button variant="outlined" onClick={reset}>Reset</Button>
---
+++ src/views/Counter/styles.js
@@ -0,0 +1,32 @@
+const styles = {
+    counter: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '30%',
+        marginLeft: '35%',
+        marginTop: '100px',
+        paddingBottom: '30px',
+        border: '2px solid #1976d2',
+        borderRadius: '5%',
+        backgroundColor: '#f8ebfa',
+
+        h1: { 
+            fontSize: '600%',
+            textAlign: 'center',
+            color: '#1976d2'
+        },
+
+        h2: { 
+            textAlign: 'center',
+            color: '#1976d2'
+        }
+    },
+
+    buttonsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    }
+}
+
+export default styles
